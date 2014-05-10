850f7210ef929f1928eed087dd1f153dfc2ec8c7
Juuso Haavisto <juuso@mail.com>
2014-05-10 23:32:22 +0300

    feature: add ResolveAndWrite function

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strings"
 	"time"
+	"fmt"
 )
 
 //Alive stands for boolean whether the proxy responded in Resolve function.
@@ -30,6 +31,39 @@ func Resolve(status chan Proxy, host string, timeout time.Duration) {
 
 	t0 := time.Now()
 	_, err := net.DialTimeout("tcp", host, timeout)
+	if err != nil {
+		proxy.ResponseTime = time.Now().Sub(t0)
+		proxy.Alive = false
+		status <- proxy
+		return
+	}
+
+	proxy.ResponseTime = time.Now().Sub(t0)
+	proxy.Alive = true
+	status <- proxy
+	return
+}
+
+//Same as Resolve function, but takes in extra parameter which defines what HTTP site the
+//proxy will attempt to connect with HEAD. The timeout in both TCP and HTTP connections are the same.
+//Useful for checking whether a proxy is busy or not.
+func ResolveAndWrite(status chan Proxy, host string, timeout time.Duration, endpoint string) {
+
+	var proxy Proxy
+	proxy.Address = host
+
+	t0 := time.Now()
+	conn, err := net.DialTimeout("tcp", host, timeout)
+	if err != nil {
+		proxy.ResponseTime = time.Now().Sub(t0)
+		proxy.Alive = false
+		status <- proxy
+		return
+	}
+
+	conn.SetReadDeadline(time.Now().Add(timeout))
+	fmt.Fprintf(conn, "HEAD "+ endpoint +" HTTP/1.0\r\n\r\n")
+	_, err = bufio.NewReader(conn).ReadString('\n')
 	if err != nil {
 		proxy.ResponseTime = time.Now().Sub(t0)
 		proxy.Alive = false
