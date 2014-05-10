d1f95db230ee7372790fcab03c2e1c42695e0ec9
Juuso Haavisto <juuso@mail.com>
2014-05-10 14:53:13 +0300

    example: add absolute file path

--- example/main.go
+++ example/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"log"
 	"proksi"
 	"time"
+	"path/filepath"
+	"os"
 )
 
 func main() {
+
+    dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
 
 	proxychan := make(chan proksi.Proxy)
 	lines := 0
@@ -18,9 +25,10 @@ func main() {
 	var timeout = time.Second * 5
 	var buffer bytes.Buffer
 
-	proxies, err := proksi.ReadFile("./proxies.txt")
+	proxies, err := proksi.ReadFile(dir+"/proxies.txt")
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	for _, proxy := range proxies {
@@ -41,10 +49,11 @@ func main() {
 		if lines == 0 {
 			err := proksi.WriteFile(time.Now().String()+".txt", buffer)
 			if err != nil {
-				log.Println(err)
+				fmt.Println(err)
+				os.Exit(1)
 			}
-			fmt.Println("Checked", amountOf, "proxies in", time.Now().Sub(te), "of which", alive, "responded.")
-			return
+			fmt.Println("Checked", amountOf, "proxies in", time.Now().Sub(te), "and of which", alive, "responded.")
+			os.Exit(0)
 		}
 	}
 }
