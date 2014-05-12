48827997db180bce6fbd26e817d733d027fca878
Juuso Haavisto <juuso@mail.com>
2014-05-12 10:22:56 +0300

    doc: fix documentation syntax for godoc, go fmt

--- main.go
+++ main.go
@@ -3,27 +3,27 @@ package proksi
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"net"
 	"os"
 	"strings"
 	"time"
-	"fmt"
 )
 
-//Alive stands for boolean whether the proxy responded in Resolve function.
-//ResponseTime is time it took for the instance to respond.
-//Address is (for now) a string representing the IP address of the proxy to be connected.
+// Alive stands for boolean whether the proxy responded in Resolve function.
+// ResponseTime is time it took for the instance to respond.
+// Address is (for now) a string representing the IP address of the proxy to be connected.
 type Proxy struct {
 	Alive        bool
 	ResponseTime time.Duration
 	Address      string
 }
 
-//Resolve makes TCP connection to given host with given timeout.
-//The function broadcasts to given channel about the response of the connection.
-//Even if the connection is refused or it times out, the channel will receive
-//new proxy instance with filled fields.
+// Resolve makes TCP connection to given host with given timeout.
+// The function broadcasts to given channel about the response of the connection.
+// Even if the connection is refused or it times out, the channel will receive
+// new proxy instance with filled fields.
 func Resolve(status chan Proxy, host string, timeout time.Duration) {
 
 	var proxy Proxy
@@ -44,9 +44,9 @@ func Resolve(status chan Proxy, host string, timeout time.Duration) {
 	return
 }
 
-//Same as Resolve function, but takes in extra parameter which defines what HTTP site the
-//proxy will attempt to connect with HEAD. The timeout in both TCP and HTTP connections are the same.
-//Useful for checking whether a proxy is busy or not.
+// Same as Resolve function, but takes in extra parameter which defines what HTTP site the
+// proxy will attempt to connect with HEAD. The timeout in both TCP and HTTP connections are the same.
+// Useful for checking whether a proxy is busy or not.
 func ResolveAndWrite(status chan Proxy, host string, timeout time.Duration, endpoint string) {
 
 	var proxy Proxy
@@ -62,7 +62,7 @@ func ResolveAndWrite(status chan Proxy, host string, timeout time.Duration, endp
 	}
 
 	conn.SetReadDeadline(time.Now().Add(timeout))
-	fmt.Fprintf(conn, "HEAD "+ endpoint +" HTTP/1.0\r\n\r\n")
+	fmt.Fprintf(conn, "HEAD "+endpoint+" HTTP/1.0\r\n\r\n")
 	_, err = bufio.NewReader(conn).ReadString('\n')
 	if err != nil {
 		proxy.ResponseTime = time.Now().Sub(t0)
@@ -77,8 +77,8 @@ func ResolveAndWrite(status chan Proxy, host string, timeout time.Duration, endp
 	return
 }
 
-//ReadFile returns string array of IP addresses in a file. It uses scanner.Scan()
-//to parse IP's from the given file.
+// ReadFile returns string array of IP addresses in a file. It uses scanner.Scan()
+// to parse IP's from the given file.
 func ReadFile(filename string) ([]string, error) {
 
 	var proxies []string
@@ -98,8 +98,8 @@ func ReadFile(filename string) ([]string, error) {
 	return proxies, nil
 }
 
-//WriteFile writes given buffer to given filename. It creates the file
-//it is not yet created. The created file uses 0600 permissions by default.
+// WriteFile writes given buffer to given filename. It creates the file
+// it is not yet created. The created file uses 0600 permissions by default.
 func WriteFile(filename string, buffer bytes.Buffer) error {
 
 	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0600)
